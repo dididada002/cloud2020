4d92f8dbf79a76e5169cbc69da4c15dd6338c451
jingteng02 <17610300902@163.com>
2020-04-14 23:15:31 +0800

    整合订单模块注册Eureka

--- cloud-cunsumer-order8661/src/main/java/com/jt/springcloud/OrderMain8661.java
+++ cloud-cunsumer-order8661/src/main/java/com/jt/springcloud/OrderMain8661.java
@@ -2,12 +2,14 @@ package com.jt.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author: jingteng
  * @date: 2020/4/14 21:22
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain8661 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain8661.class,args);
