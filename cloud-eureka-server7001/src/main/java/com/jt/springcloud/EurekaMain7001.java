aa2a4554093842bfa3caaf984a62624b2269ceae
jingteng02 <17610300902@163.com>
2020-04-14 22:59:36 +0800

    创建Eureka Server模块

---
+++ cloud-eureka-server7001/src/main/java/com/jt/springcloud/EurekaMain7001.java
@@ -0,0 +1,17 @@
+package com.jt.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: jingteng
+ * @date: 2020/4/14 22:55
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class);
+    }
+}
