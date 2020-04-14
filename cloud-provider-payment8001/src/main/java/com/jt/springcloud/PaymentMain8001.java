f1a9862dfbde94dafc5c35dc19964aa9110e2f70
jingteng02 <17610300902@163.com>
2020-04-14 23:09:16 +0800

    整合支付模块注册Eureka

--- cloud-provider-payment8001/src/main/java/com/jt/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/jt/springcloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package com.jt.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author: jingteng
  * @date: 2020/4/13 22:25
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
