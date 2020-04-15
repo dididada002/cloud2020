764cabd28a679ed1adc8df6a010f6389b6a6cde7
jingteng02 <17610300902@163.com>
2020-04-15 22:35:15 +0800

    Eureka提代方案zookeeper模块创建-未跑通，暂缓

---
+++ cloud-provider-payment8004/src/main/java/com/jt/springcloud/PaymentMain8004.java
@@ -0,0 +1,17 @@
+package com.jt.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author: jingteng
+ * @date: 2020/4/15 22:17
+ */
+@SpringBootApplication
+@EnableDiscoveryClient //这个注解用于向使用consul或者zookeeper作为注册中心时注册服务
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class,args);
+    }
+}
---
+++ cloud-provider-payment8004/src/main/java/com/jt/springcloud/controller/PayController.java
@@ -0,0 +1,11 @@
+package com.jt.springcloud.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: jingteng
+ * @date: 2020/4/15 22:18
+ */
+@RestController
+public class PayController {
+}
