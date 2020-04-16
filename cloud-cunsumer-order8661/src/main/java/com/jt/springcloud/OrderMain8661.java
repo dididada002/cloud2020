3cf9b28a842397eb2c9d8508043f37fd83ecb410
jingteng02 <17610300902@163.com>
2020-04-16 14:28:41 +0800

    Ribbon替换负载规则-增加负载配置类、启动类加注解

--- cloud-cunsumer-order8661/src/main/java/com/jt/springcloud/OrderMain8661.java
+++ cloud-cunsumer-order8661/src/main/java/com/jt/springcloud/OrderMain8661.java
@@ -1,8 +1,10 @@
 package com.jt.springcloud;
 
+import com.jt.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @author: jingteng
@@ -10,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
 public class OrderMain8661 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain8661.class,args);
