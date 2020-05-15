dd7edb5bcfb2510fcb33d4cf34aca88990a6fa31
lizhuo <1023191642@qq.com>
2020-05-15 22:46:25 +0800

    nacos-ribbon负载均衡8011

---
+++ cloud-provider-nacos-payment8011/src/main/java/com/jt/springcloud/NacosPaymentMain8011.java
@@ -0,0 +1,17 @@
+package com.jt.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author: jingteng
+ * @date: 2020/4/13 22:25
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosPaymentMain8011 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosPaymentMain8011.class,args);
+    }
+}
---
+++ cloud-provider-nacos-payment8011/src/main/java/com/jt/springcloud/interfaces/PaymentMapper.java
@@ -0,0 +1,33 @@
+package com.jt.springcloud.interfaces;
+
+import com.jt.springcloud.entity.Payment;
+import com.jt.springcloud.entity.PaymentExample;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface PaymentMapper {
+    long countByExample(PaymentExample example);
+
+    int deleteByExample(PaymentExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Payment record);
+
+    int insertSelective(Payment record);
+
+    List<Payment> selectByExample(PaymentExample example);
+
+    Payment selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Payment record, @Param("example") PaymentExample example);
+
+    int updateByExample(@Param("record") Payment record, @Param("example") PaymentExample example);
+
+    int updateByPrimaryKeySelective(Payment record);
+
+    int updateByPrimaryKey(Payment record);
+}
