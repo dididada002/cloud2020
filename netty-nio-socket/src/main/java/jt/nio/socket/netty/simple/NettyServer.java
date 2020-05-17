67e273de54a7a478f0f5b8389f26062795419e9a
jingteng02 <17610300902@163.com>
2020-05-17 16:27:32 +0800

    netty-netty简单模型：客户端以及处理器、服务器端以及处理器

---
+++ netty-nio-socket/src/main/java/jt/nio/socket/netty/simple/NettyServer.java
@@ -0,0 +1,56 @@
+package jt.nio.socket.netty.simple;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @author: jingteng
+ * @date: 2020/5/17 0:42
+ */
+public class NettyServer {
+    public static void main(String[] args) throws InterruptedException {
+        //创建BossGroup 和 WorkerGroup
+        /**
+         * 含有的子线程(NioEventLoop)数：
+         *      默认是实际CPU核数 * 2
+         *      可以指定
+         * */
+        EventLoopGroup boosGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            //创建服务器端的启动对象，并配置
+            ServerBootstrap bootstrap = new ServerBootstrap();
+
+            //配置
+            bootstrap.group(boosGroup,workerGroup)//设置两个线程组
+                    .channel(NioServerSocketChannel.class)//使用NioServerSocketChannel作为服务器的通道实现
+                    .option(ChannelOption.SO_BACKLOG,128)//设置线程队列得到连接个数
+                    .childOption(ChannelOption.SO_KEEPALIVE,true)//设置保持活动连接状态
+                    .childHandler(new ChannelInitializer<SocketChannel>() {//创建一个通道初始对象
+                        //给pipeline设置处理器
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new NettyServerHandler());
+                        }
+                    });//给WorkerGroup的EventLoop对应的管道设置处理器
+
+            System.out.println("服务器准备好了。。。");
+
+            //启动服务器：绑定一个端口并且同步，生成一个ChannelFuture对象
+            ChannelFuture cf = bootstrap.bind(6668).sync();
+
+            //对关闭通道进行监听
+            cf.channel().closeFuture().sync();
+        } finally {
+            boosGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
