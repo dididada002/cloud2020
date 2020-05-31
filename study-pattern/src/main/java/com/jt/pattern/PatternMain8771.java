46e0194e215f241dfe48a56eaffd9bebd73e892e
jingteng02 <17610300902@163.com>
2020-05-31 14:51:59 +0800

    设计模式-单例6种实现

---
+++ study-pattern/src/main/java/com/jt/pattern/PatternMain8771.java
@@ -0,0 +1,15 @@
+package com.jt.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: jingteng
+ * @date: 2020/5/24 14:01
+ */
+@SpringBootApplication
+public class PatternMain8771 {
+    public static void main(String[] args) {
+        SpringApplication.run(PatternMain8771.class,args);
+    }
+}
