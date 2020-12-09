e276dc9589fa986c5778277b1f289728b13177d4
jingteng <jingteng@kaikeba.com>
2020-12-09 17:32:55 +0800

    基础工具功能demo

---
+++ utils-jt/src/main/java/com/jt/demo/UtilsDemoMain8848.java
@@ -0,0 +1,15 @@
+package com.jt.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: jingteng
+ * @date: 2020/12/9 5:27 下午
+ */
+@SpringBootApplication
+public class UtilsDemoMain8848 {
+    public static void main(String[] args) {
+        SpringApplication.run(UtilsDemoMain8848.class);
+    }
+}
