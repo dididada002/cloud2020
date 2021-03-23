a38370af68fea112e1e25ba1f750f73fba7618b1
jingteng <jingteng@kaikeba.com>
2021-03-23 16:37:42 +0800

    leetcode -- 相同的树

---
+++ leetcode-jt/src/main/java/leetcode/editor/cn/相同的树.java
@@ -0,0 +1,104 @@
+package leetcode.editor.cn;
+
+//给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。 
+//
+// 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：p = [1,2,3], q = [1,2,3]
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：p = [1,2], q = [1,null,2]
+//输出：false
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：p = [1,2,1], q = [1,1,2]
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 两棵树上的节点数目都在范围 [0, 100] 内 
+// -104 <= Node.val <= 104 
+// 
+// Related Topics 树 深度优先搜索 
+// 👍 585 👎 0
+
+public class 相同的树{
+	public static void main(String[] args) {
+		Solution solution = new 相同的树().new Solution();
+		
+	}
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        //前序遍历
+        if (p == null && q != null){
+            return false;
+        }
+        if (p != null && q == null){
+            return false;
+        }
+        if (p == null && q == null){
+            return true;
+        }
+        if (p.val != q.val){
+            return false;
+        }
+        //左子节点
+        boolean sameLeftTree = isSameTree(p.left, q.left);
+        if (!sameLeftTree){
+            return false;
+        }
+        //右子节点
+        boolean sameRightTree = isSameTree(p.right, q.right);
+        if (!sameRightTree){
+            return false;
+        }
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
