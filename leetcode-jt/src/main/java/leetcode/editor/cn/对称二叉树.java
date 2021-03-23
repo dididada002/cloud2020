238c2a3a58851258e3468f1e62f50e3ce7b72889
jingteng <jingteng@kaikeba.com>
2021-03-23 17:22:01 +0800

    leetcode -- 对称二叉树

---
+++ leetcode-jt/src/main/java/leetcode/editor/cn/对称二叉树.java
@@ -0,0 +1,88 @@
+package leetcode.editor.cn;
+
+//给定一个二叉树，检查它是否是镜像对称的。 
+//
+// 
+//
+// 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。 
+//
+//     1
+//   / \
+//  2   2
+// / \ / \
+//3  4 4  3
+// 
+//
+// 
+//
+// 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的: 
+//
+//     1
+//   / \
+//  2   2
+//   \   \
+//   3    3
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 你可以运用递归和迭代两种方法解决这个问题吗？ 
+// Related Topics 树 深度优先搜索 广度优先搜索 
+// 👍 1300 👎 0
+
+public class 对称二叉树{
+	public static void main(String[] args) {
+		Solution solution = new 对称二叉树().new Solution();
+		
+	}
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode() {}
+	TreeNode(int val) { this.val = val; }
+	TreeNode(int val, TreeNode left, TreeNode right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+}
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+		return checkSameTree(root.left,root.right);
+    }
+    public boolean checkSameTree(TreeNode left,TreeNode right){
+    	if (left == null && right != null){
+    		return false;
+		}
+    	if (left != null && right == null){
+    		return false;
+		}
+    	if (left == null){
+    		return true;
+		}
+    	return (left.val == right.val) && checkSameTree(left.left,right.right) && checkSameTree(left.right,right.left);
+	}
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
