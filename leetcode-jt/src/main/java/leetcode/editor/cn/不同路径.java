5ebe3e42d57d8641f53bd6a5a8f52aef3dc696fa
jingteng <jingteng@kaikeba.com>
2021-11-16 14:33:51 +0800

    leetcode -- 不同路径

---
+++ leetcode-jt/src/main/java/leetcode/editor/cn/不同路径.java
@@ -0,0 +1,95 @@
+package leetcode.editor.cn;
+
+//一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。 
+//
+// 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。 
+//
+// 问总共有多少条不同的路径？ 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：m = 3, n = 7
+//输出：28 
+//
+// 示例 2： 
+//
+// 
+//输入：m = 3, n = 2
+//输出：3
+//解释：
+//从左上角开始，总共有 3 条路径可以到达右下角。
+//1. 向右 -> 向下 -> 向下
+//2. 向下 -> 向下 -> 向右
+//3. 向下 -> 向右 -> 向下
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：m = 7, n = 3
+//输出：28
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：m = 3, n = 3
+//输出：6 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= m, n <= 100 
+// 题目数据保证答案小于等于 2 * 109 
+// 
+// Related Topics 数学 动态规划 组合数学 
+// 👍 1169 👎 0
+
+public class 不同路径{
+	public static void main(String[] args) {
+		Solution solution = new 不同路径().new Solution();
+		System.out.println(solution.uniquePaths(51, 9));
+	}
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int uniquePaths(int m, int n) {
+		/**
+		 * 递归会出现大量的重复计算，所以重复结果需要记录
+		 */
+		/*
+		if (m == 1 && n == 1){
+			return 1;
+		}
+		if (m == 1){
+			return uniquePaths(1,n-1);
+		}
+		if (n == 1){
+			return uniquePaths(m - 1,1);
+		}
+		return uniquePaths(m,n-1) + uniquePaths(m-1,n);
+		 */
+		//动态规划
+		int[][] resultInt = new int[m][n];
+		for (int i = 0; i < m; i++) {
+			resultInt[i][0] = 1;
+		}
+		for (int i = 0; i < n; i++) {
+			resultInt[0][i] = 1;
+		}
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				resultInt[i][j] = resultInt[i - 1][j] + resultInt[i][j - 1];
+			}
+		}
+		return resultInt[m-1][n-1];
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
